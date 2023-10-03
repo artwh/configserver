4e2bc288569e5086fd7fae747610da49ba1d7bea
Artem <artemij.belozertsev@yandex.ru>
2023-10-03 22:45:33 +0400

    commit

---
+++ src/main/java/com/example/configserver/configuration/Config.java
@@ -0,0 +1,13 @@
+package com.example.configserver.configuration;
+
+
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+@Data
+@Configuration
+public class Config {
+    @Value("${my.property.test}")
+    private String testProperty;
+}
